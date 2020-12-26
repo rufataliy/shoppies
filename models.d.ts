f6cb5b55b4453ffe819b0dde1cedb109de5b8137
Rufat Aliyev <46456022+rufataliy@users.noreply.github.com>
2020-12-26 18:08:20 -0500

    Added models

---
+++ models.d.ts
@@ -0,0 +1,20 @@
+type Movie = {
+  Title: string;
+  Year: string;
+  imdbID: string;
+  Type: string;
+  Poster: string;
+};
+
+type ContextDefault = {
+  addNomination: (id: string) => void;
+  removeNomination: (id: string) => void;
+  nominationList: Movie[] | null;
+  nominationLoading: boolean;
+  searchMovies: (params: SearchParams) => void;
+  movieList: Movie[] | null;
+  movieLoading: boolean;
+  getNominationList: () => void;
+};
+
+type SearchParams = { s: string; type?: string; year?: string; t?: string };
