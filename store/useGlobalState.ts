56cd31e6d62dee8e3d040bb000f57e3db4a2fc38
Rufat Aliyev <46456022+rufataliy@users.noreply.github.com>
2020-12-26 22:11:07 -0500

    Added method for getting nominated movies

--- store/useGlobalState.ts
+++ store/useGlobalState.ts
@@ -1,14 +1,39 @@
 import { useState } from "react";
+import { getNominatedMovieIds } from "../helpers/getNominatedMovieIds";
+import { makeApiRequest } from "../helpers/makeApiRequest";
+import {API_BYIDS } from "../constants";
+
+const makeNominationMovieIdsString = () => {
+  if (getNominatedMovieIds()) {
+    return getNominatedMovieIds().join(",");
+  } else {
+    return "";
+  }
+};
+
 export const useGlobalState = (): ContextDefault => {
   const [nominationList, setNominationList] = useState<Movie[] | null>(null);
   const [movieList, setMovieList] = useState<Movie[] | null>(null);
   const [nominationLoading, setNominationLoading] = useState(false);
   const [movieLoading, setMovieLoading] = useState(false);
 
+  const getNominationList = () => {
+    if (Boolean(makeNominationMovieIdsString())) {
+      makeApiRequest(
+        `${API_BYIDS}${getNominatedMovieIds()}`,
+        (data) => setNominationList(data),
+        (status: boolean) => setNominationLoading(status)
+      );
+    } else {
+      setNominationList(null);
+    }
+  };
+
   return {
     nominationList,
     nominationLoading,
     movieList,
     movieLoading,
+    getNominationList,
   };
 };
